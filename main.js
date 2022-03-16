a1d82b687ceb34536810e2bc34f9309027db197f
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-16 16:03:03 +0000

    Ri-Puscio

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 // SECTION - Snack1 - Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
-// let x = parseInt  (prompt("Inserisci nummero"));
+let x = parseInt  (prompt("Inserisci nummero"));
 // NOTE Come usare il isNUN e tornare indietro
-// if (x % 2 == 0) {
-// document.getElementById("numero").innerHTML = `Il numero ${x} è pari!`;
-// }
-// else {
-// console.log(x);
-// alert ("Sbagliato")
-// document.getElementById("numero").innerHTML = `Il numero ${x} non è pari!`;
-// }
+if (x % 2 == 0) {
+document.getElementById("numero").innerHTML = `Il numero ${x} è pari!`;
+}
+else {
+console.log(x);
+alert ("Sbagliato")
+document.getElementById("numero").innerHTML = `Il numero ${x} non è pari!`;
+}
 // !SECTION
 
 // SECTION - Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
 
-// let nome = ["Minny", "Pippo", "Pluto", "Paperino", "Qui", "Quo", "Qua",];
-// let cognome = ["Bassettini", "Paperone", "Paperon", "Paperin", "Truzzo", "Truzzone", "Truzzetto",];
+let nome = ["Minny", "Pippo", "Pluto", "Paperino", "Qui", "Quo", "Qua",];
+let cognome = ["Bassettini", "Paperone", "Paperon", "Paperin", "Truzzo", "Truzzone", "Truzzetto",];
 
-// let x = nome [ Math.floor (Math.random( ) * 6 ) ]
-// let y = cognome [ Math.floor (Math.random( ) * 6 ) ]
+let x = nome [ Math.floor (Math.random( ) * 6 ) ]
+let y = cognome [ Math.floor (Math.random( ) * 6 ) ]
 
-// document.getElementById("nome").innerHTML= x
-// document.getElementById("cognome").innerHTML= y
+document.getElementById("nome").innerHTML= x
+document.getElementById("cognome").innerHTML= y
 // !SECTION
 
 // SECTION - Snack3 -Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
