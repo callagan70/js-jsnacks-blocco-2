f7218f98640a14725ed9d3bc85d7b9f4e2a9881a
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-16 19:34:11 +0000

    Codice pulito e aposto, vedere console

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 let x = parseInt  (prompt("Inserisci un numero pari"));
 
 if (x % 2 == 0) {
-document.getElementById("numero").innerHTML = `Il numero ${x} è pari!`;}
+        document.getElementById("numero").innerHTML = `Il numero ${x} è pari!`;}
 
 else {
         alert ("Sbagliato")
@@ -24,21 +24,24 @@ document.getElementById("cognome").innerHTML= yCognome;
 
 // SECTION - Snack3 -Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
 
- var array = []
+var array = []
+let z =  Math.floor (Math.random( ) * 10 + 10 )
 
- let z =  Math.floor (Math.random( ) * 10 + 10 )
-
- for (i = 1; i<=z; i++ ){
+for (i = 1; i<=z; i++ ){
     let k =  Math.floor (Math.random( ) * 100 + 1 );
     k = array.push(k);}
+console.log("Console.log del 3° snecchino")
+console.log(array)
+console.log("------")
 
- var x1 = 0
+var x1 = 0
 
- for (i = 0; i < array.length; i++){
+for (i = 0; i < array.length; i++){
         if (i % 2 == 0) {}
         else {x1 += array[i]}
  }
- document.getElementById("sommArray").innerHTML = `Somma Array: ${x1}`
+
+document.getElementById("sommArray").innerHTML = `Somma Array: ${x1}`
 // !SECTION
 
 // SECTION Snack4 (Bonus) Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
@@ -51,13 +54,15 @@ let zSnack4_1 =  Math.floor (Math.random( ) * 10 + 10 )
  for (i = 1; i <= zSnack4_1; i++ ){
     let kSnack4 =  Math.floor (Math.random( ) * 100 + 1 )
     kSnack4 = array1Snack4.push(kSnack4);}
-                      console.log(array1Snack4)
+                    console.log("1° array random del 4° snecchino")                  
+                    console.log(array1Snack4)
 
 let zSnack4_2 =  Math.floor (Math.random( ) * 10 + 10 )
 
 for (i = 1; i <= zSnack4_2; i++ ){
     let kSnack4 =  Math.floor (Math.random( ) * 100 + 1 )
     kSnack4 = array2Snack4.push(kSnack4);}
+                      console.log("2° array random del 4° snecchino")
                       console.log(array2Snack4)
 
 let difArray = array1Snack4.length - array2Snack4.length;
@@ -69,18 +74,19 @@ else if (difArray < 0){
         document.getElementById("differenza").innerHTML = `Il primo array ha un length inferiore rispetto al secondo di ${difArray}`;
         for (i=1; i <= difArray; i++){
           let k =  Math.floor (Math.random( ) * 100 + 1 );
-          k = array1Snack4.push(k)
+          k = array1Snack4.push(k);
         document.getElementById("aggiunti").innerHTML = `Aggiunti ${difArray} ora il primo array è di ${array1Snack4.length} uguale al secondo`;}
-}
+        }
 else {
         document.getElementById("differenza").innerHTML = `Il secondo array ha un length inferiore rispetto al secondo di ${difArray}`;
         for (i=1; i <= difArray; i++){
           let k =  Math.floor (Math.random( ) * 100 + 1 );
-          k = array2Snack4.push(k)
+          k = array2Snack4.push(k);
         document.getElementById("aggiunti").innerHTML = `Aggiunti ${difArray} ora il secondo array è di ${array2Snack4.length} uguale al primo`;}
       }
-
-      console.log(`Primo array ${array1Snack4}`)
-      console.log(`Secondo array ${array2Snack4}`)
-
+                          console.log("------")
+                          console.log("1° array del 4° snecchino dopo il ciclo if di controllo")
+                          console.log(array1Snack4)
+                          console.log("2° array del 4° snecchino dopo il ciclo if di controllo")
+                          console.log(array2Snack4)
 // !SECTION
