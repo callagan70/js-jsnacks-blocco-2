25a849bd37dffd9e248bea9565ba113d2ed08f10
Angelo Melotti <96485963+callagan70@users.noreply.github.com>
2022-03-16 17:22:19 +0000

    Fatto ! v1.01

--- main.js
+++ main.js
@@ -59,4 +59,4 @@ else {
  document.getElementById("sommArray").innerHTML = `Somma Array: ${x1}`
 // !SECTION
 
-
+// SECTION Snack4 (Bonus) Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
